efa8ad3db35adf33a6bd2934d5510b64eb0fda7c
Tarek Yakoubi <42013242@parisnanterre.fr>
2022-11-01 20:16:25 +0100

    Kenko Starter

---
+++ src/main/java/com/kenkogroup/kenko/KenkoApplication.java
@@ -0,0 +1,13 @@
+package com.kenkogroup.kenko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KenkoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KenkoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kenkogroup/kenko/KenkoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kenkogroup.kenko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KenkoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
