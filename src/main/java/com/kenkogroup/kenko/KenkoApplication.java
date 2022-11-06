b83c0c54d353c0e8aa462bb7c0a744756fe3a0d4
Tarek Yakoubi <42013242@parisnanterre.fr>
2022-11-06 22:56:14 +0100

    feat: added CRUD MockServiceTests

--- src/main/java/com/kenkogroup/kenko/KenkoApplication.java
+++ src/main/java/com/kenkogroup/kenko/KenkoApplication.java
@@ -1,6 +1,5 @@
 package com.kenkogroup.kenko;
 
-import com.kenkogroup.kenko.user.entity.User;
 import com.kenkogroup.kenko.user.repository.UserRepository;
 import com.kenkogroup.kenko.user.service.UserService;
 import com.kenkogroup.kenko.user.controller.UserController;
@@ -9,10 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.stereotype.Component;
 
-import javax.persistence.Entity;
 
 @ComponentScan(basePackageClasses = UserService.class)
 @ComponentScan(basePackageClasses = UserRepository.class)
