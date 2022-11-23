0612ef3e3f1956541c7d670e049b352c8d010948
KenzaSAD <99667226+KenzaSAD@users.noreply.github.com>
2022-11-24 00:08:11 +0100

    ajout button

--- frontend/src/component/Ingredients.js
+++ frontend/src/component/Ingredients.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Button from 'react-bootstrap/Button';
 const PRODUCTS =[
     {category: "Vegetable", name : "TOMATO", img: "./component/tomate.jpg"},
     {category: "Vegetable", name : "POTATO", img:"/IMG_20220704_133559.jpg"},
@@ -18,25 +19,25 @@ class Ingredients extends Component {
 render(){
     console.log("Products",PRODUCTS)
 return (
-    <>
-    <h1>Ingredients : </h1>
-            {PRODUCTS.map((product)=>(
-                <div>
-                <img alt="" src ="./component/tomate.jpg"></img>
-                <br></br>
-                <span>Categorie : {product.category}</span>
-                <br></br>
-                <span>Nom : {product.name}</span>
-                <br></br>
-                <label>Quantité :    </label>
-                <input></input>
-                <br></br>
-                <br></br>
-                </div>
-            ))}
-    </>
+   <>
+       <h1>Ingredients : </h1>
+               {PRODUCTS.map((product)=>(
+                   <div>
+                   <br></br>
+                   <br></br>
+                   <span>Categorie : {product.category}</span>
+                   <br></br>
+                   <span>Nom : {product.name}</span>
+                   <br></br>
+                   <lable>Quantité :    </lable>
+                   <input></input>
+                   <br></br>
+                   <Button> Ajouter</Button>
+                   </div>
+               ))}
+       </>
 
-    );
+       );
 }
 }
 export default Ingredients;
