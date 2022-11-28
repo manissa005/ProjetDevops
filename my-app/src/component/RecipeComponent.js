4fba7d3b0d0b8695f9a735f3b2f85554561e84cf
Tarek Yakoubi <42013242@parisnanterre.fr>
2022-11-28 22:04:37 +0100

    petite feature 1

--- my-app/src/component/RecipeComponent.js
+++ my-app/src/component/RecipeComponent.js
@@ -2,37 +2,66 @@ import React, { Component } from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import ListGroup from 'react-bootstrap/ListGroup';
-import SearchService from '../services/SearchService';
-import { useState } from 'react';
+import SearchRecipeByNumberAndDuration from '../services/SearchRecipeByNumberAndDuration';
+
 class RecipeComponent extends Component {
     constructor(props) {
         super(props)
         this.state = {
             recipe: [],
-            s: []
+            s: [],
+            resultRecipe: "",
+            recipeNumber: 1,
+            duration: 0
         }
     }
 
-
-
     render() {
         return (
-            <div className="row">
-                <div><form className="search" >
+            <div>
+                <form>
+                    <label>Ingredients</label>
                     <input type="text" placeholder="search" name="search" onChange={async (event) => {
-                        const { search } = SearchService();
                         const value = event.target.value;
-
-                        try {
-                            const data = await search(value);
-                            this.setState({ s: data.data });
-                        }
-                        catch (error) {
-                            console.log(error);
-                        }
+                        this.setState({ resultRecipe: value });
+                    }} />
+                    <br></br>
+                    <label>Recipe Number</label>
+                    <input type="text" placeholder="Recipe Number" onChange={async (event) => {
+                        const value = event.target.value;
+                        this.setState({ recipeNumber: value });
+                    }} />
+                    <br></br>
+                    <label>Duration</label>
+                    <input type="text" placeholder="Max Duration" onChange={async (event) => {
+                        const value = event.target.value;
+                        this.setState({ duration: value });
                     }} />
                 </form>
-                </div>
+                <Button onClick={async () => {
+                    /* try {
+                         const { recipeNumber } = numberOfRecipeService();
+                         const data = await recipeNumber(this.state.resultRecipe, this.state.recipeNumber);
+                         this.setState({ s: data.data });
+                         this.setState({ recipeNumber: 1 });
+                     }
+                     catch (error) {
+                         console.log(error);
+                     }*/
+
+                    try {
+                        const { recipe } = SearchRecipeByNumberAndDuration();
+                        const data = await recipe(this.state.resultRecipe, this.state.recipeNumber, this.state.duration);
+                        this.setState({ s: data.data });
+                        this.setState({ recipeNumber: 1 });
+                        console.log(data);
+
+                    }
+                    catch (error) {
+                        console.log(error);
+                    }
+
+                }} >Valider</Button>
                 {
                     this.state.s.map(
                         s =>
@@ -46,12 +75,12 @@ class RecipeComponent extends Component {
                                 </Card.Body>
 
                                 <ListGroup className="list-group-flush">
-                                    <ListGroup.Item><b>PrÃ©partion</b></ListGroup.Item>
+                                    <ListGroup.Item><b>Prépartion</b></ListGroup.Item>
                                     {s.ingredientLines.map(line =>
                                         <ListGroup.Item><li>{line}</li></ListGroup.Item>
                                     )}
 
-                                    <ListGroup.Item><b>DurÃ©e</b></ListGroup.Item>
+                                    <ListGroup.Item><b>Durée</b></ListGroup.Item>
                                     <ListGroup.Item>{s.totalTime}minutes</ListGroup.Item>
 
                                     <ListGroup.Item><b>Calories</b></ListGroup.Item>
@@ -66,11 +95,15 @@ class RecipeComponent extends Component {
                                 </Card.Body>
                             </Card>
                     )
+
                 }
             </div >
         );
+
     }
+
 }
+
 
 
 export default RecipeComponent;
