5a0a888643cab1fbcb7eea88ae9db7d2447671ac
AniesseMehenaoui <114982268+AniesseMehenaoui@users.noreply.github.com>
2023-01-23 14:43:29 +0100

    Delete src directory

--- src/main/java/com/kenkogroup/kenko/recipe/service/RecipeService.java
+++
@@ -1,7 +0,0 @@
-package com.kenkogroup.kenko.recipe.service;
-
-import com.kenkogroup.kenko.recipe.entity.edamam.Example;
-
-public interface RecipeService {
-	double CaloriesCalculator(String age, String weight, String tall, String sexe);
-}
--- src/test/java/com/kenkogroup/kenko/KenkoApplicationTests.java
+++
@@ -1,6 +0,0 @@
-package com.kenkogroup.kenko;
-import org.springframework.boot.test.context.SpringBootTest;
-@SpringBootTest
-class KenkoApplicationTests {
-
-}
