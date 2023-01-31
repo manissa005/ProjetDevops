901e9fa70b9246d5eb8a527e89c1c97111b9b2e1
KenzaSAD <99667226+KenzaSAD@users.noreply.github.com>
2023-01-31 10:18:38 +0100

    Feature recommandation de recettes en focntion des likes + liker une recette

--- my-app/src/services/RecipeService.js
+++ my-app/src/services/RecipeService.js
@@ -7,8 +7,18 @@ const create = data => {
     return http.post("/analyse", data);
 };
 
+const getLike = () => {
+    return http.get("/getLike");
+};
+
+const postLike = id => {
+    return http.get("/postLike/"+ id);
+};
+
 const RecipeService = {
     getAll,
     create,
+    getLike,
+    postLike
 };
 export default RecipeService;
