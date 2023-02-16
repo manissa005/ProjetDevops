fe5179d55c487ce40b824eae9c40588791d3080b
agnes06MHN <agnesmehenaoui@gmail.com>
2023-02-16 11:58:39 +0100

    Login Service

---
+++ my-app/src/services/LoginService.js
@@ -0,0 +1,12 @@
+import axios from "axios"
+
+function LoginService() {
+
+    const login = async (email,password) => {
+        return await axios.get('http://localhost:8081/api/v1/user/login/' + email +"/" + password)
+    };
+
+    return { login }
+}
+
+export default LoginService;
